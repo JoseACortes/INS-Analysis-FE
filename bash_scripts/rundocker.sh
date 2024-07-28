1888a6f5decf33be91a324416a7471974aafd8ed
jac2462 <jac2462@MATH-139200.uta.edu>
2024-07-27 19:13:11 -0500

    one day i will fix this

--- bash_scripts/rundocker.sh
+++ bash_scripts/rundocker.sh
@@ -1,2 +1,2 @@
-# docker run --rm -p 8501:8501 ins-analysis-fe bash bash_scripts/startserver.sh
-docker run -it --rm -p 8501:8501 ins-analysis-fe bash
+# docker run --rm -p 80:80 ins-analysis-fe bash bash_scripts/startserver.sh
+docker run -it --rm -p 8501:8501 -p 80:80 ins-analysis-fe bash
